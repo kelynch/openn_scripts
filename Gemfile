e997209f2d444ec04a454dbc77d78bdf7c93b1fa
Kate Lynch <katelynch@princeton.edu>
2020-08-07 16:43:56 -0400

    Inserting structural metadata into OPenn spreadsheet

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'json'
 gem 'pry'
 gem 'httpclient'
+gem 'rubyXL'
--- get_struct_md.rb
+++ get_struct_md.rb
@@ -5,14 +5,18 @@ require 'net/http'
 require 'json'
 require 'pry'
 require 'httpclient'
+require 'rubyXL'
 
-object_json = File.read(ARGV[0])
+manifest_filename = ARGV[0]
+object_json = File.read(manifest_filename)
 object_manifest = JSON.parse(object_json)
 client = HTTPClient.new
 serial_num = 0
 
 csv_data = []
-csv_data << ["SERIAL_NUM", "DISPLAY PAGE", "FILE_NAME"]
+
+workbook = RubyXL::Parser.parse("pages.xlsx")
+worksheet = workbook[0]
 
 object_manifest['sequences'].each do |seq|
   seq['canvases'].each do |canvas|
@@ -22,8 +26,21 @@ object_manifest['sequences'].each do |seq|
     h = client.head(tiff_url).header
     filename_response = h['Content-Disposition']
     filename = /[0-9]+.tif/.match(filename_response.first).to_s
-    csv_data << [serial_num, label, filename]
+    csv_data << [serial_num, label, filename, tiff_url]
   end
 end
 
-File.write("#{ARGV[1]}.csv", csv_data.map(&:to_csv).join)
+spreadsheet_offset = 3
+
+csv_data.each_with_index do |value, index|
+  serial_num_y_index = 0
+  display_page_y_index = 1
+  file_name_y_index = 2
+  url_y_index = 15
+  worksheet.add_cell((spreadsheet_offset+index), serial_num_y_index, value[0])
+  worksheet.add_cell((spreadsheet_offset+index), display_page_y_index, value[1])
+  worksheet.add_cell((spreadsheet_offset+index), file_name_y_index, value[2])
+  worksheet.add_cell((spreadsheet_offset+index), url_y_index, value[3])
+end
+
+workbook.write("#{manifest_filename}.xlsx")
